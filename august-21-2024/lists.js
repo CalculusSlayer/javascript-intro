8f2132a7c7986ee5c1aa61cf08d56cb95a0d1105
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-21 09:26:29 -0700

    added some push calls

--- august-21-2024/lists.js
+++ august-21-2024/lists.js
@@ -7,3 +7,14 @@ console.log();
 console.log("Delete 3 from array1");
 array1.pop();
 console.log(`array1: [${array1}]`);
+console.log();
+
+console.log("Append 4 to array1");
+array1.push(4);
+console.log(`array1: [${array1}]`);
+console.log();
+
+console.log("Append 5 to array1");
+array1.push(5);
+console.log(`array1: [${array1}]`);
+console.log();
