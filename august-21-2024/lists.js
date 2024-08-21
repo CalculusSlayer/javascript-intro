8107c4edb40acad57f3b48822f8ee9ab41671c72
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-21 09:21:15 -0700

    Initialized lists.js and added pop call

---
+++ august-21-2024/lists.js
@@ -0,0 +1,9 @@
+console.log("This is lists.js");
+
+let array1 = [1, 2, 3];
+console.log(`array1: [${array1}]`);
+console.log();
+
+console.log("Delete 3 from array1");
+array1.pop();
+console.log(`array1: [${array1}]`);
