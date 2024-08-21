51fa2dd1a0de0d4e8a54e4048e9fa1199006f0c2
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-08-21 09:54:29 -0700

    prepended an element to array

--- august-21-2024/lists.js
+++ august-21-2024/lists.js
@@ -23,3 +23,8 @@ console.log("Remove 4 from array1");
 array1.splice(2, 1);
 console.log(`array1: [${array1}]`);
 console.log();
+
+console.log("Prepend 40 in array1");
+array1.splice(0, 0, 40);
+console.log(`array1: [${array1}]`);
+console.log();
